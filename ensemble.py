30e34492d269df0bcae9b86ea52072b737c06fbb
Marco <marcovirgolin@users.noreply.github.com>
2019-01-10 12:38:22 +0100

    Update ensemble.py
    had an hard-coded path in read dataset from path

--- ensemble.py
+++ ensemble.py
@@ -21,7 +21,7 @@ from keras import backend as K
 
 
 def read_external_dataset(path):
-	dataset = np.loadtxt('datasets/rw/yacht_full.dat')
+	dataset = np.loadtxt(path)
 	dataset = preprocessing.scale(dataset)
 	n_features = dataset.shape[1] - 1
 	train, test = train_test_split(dataset, test_size=0.25, random_state=42)
